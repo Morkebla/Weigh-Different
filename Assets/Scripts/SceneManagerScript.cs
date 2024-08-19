ae6d264257a347d40a6bc74853d0fed809a41efc
Morkebla <109845755+Morkebla@users.noreply.github.com>
2024-08-19 16:51:20 +0100

    Sound,UI,Scene implementations
    Sounds play when an object is picked up. A UI button with 'Congratulations' appears when the correct amount is placed on the scale. SceneManager is implemented.

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
